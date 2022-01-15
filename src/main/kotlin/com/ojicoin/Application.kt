7ad98febd873f7e9451da9869188c43eb9bdca82
ah.jo <ah.jo@navercorp.com>
2022-01-16 01:25:36 +0900

    [#FALCON-2] ORM 설정 추가

--- src/main/kotlin/com/ojicoin/Application.kt
+++ src/main/kotlin/com/ojicoin/Application.kt
@@ -2,6 +2,7 @@ package com.ojicoin
 
 import com.ojicoin.plugins.configureRouting
 import com.ojicoin.plugins.configureSerialization
+import com.ojicoin.service.DatabaseFactory
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
@@ -9,5 +10,6 @@ fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
         configureSerialization()
+        DatabaseFactory.connectAndMigrate()
     }.start(wait = true)
 }
