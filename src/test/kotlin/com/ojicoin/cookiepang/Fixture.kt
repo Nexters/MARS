7458d6e9e1ab878fa7b679cfab1dd53e1b0288f2
ah.jo <ah.jo@navercorp.com>
2022-02-02 18:25:44 +0900

    [Nexters/FALCON#3] RestAssured 와 컨트롤러 테스트 추가

--- src/test/kotlin/com/ojicoin/cookiepang/Fixture.kt
+++ src/test/kotlin/com/ojicoin/cookiepang/Fixture.kt
@@ -2,13 +2,25 @@ package com.ojicoin.cookiepang
 
 import com.navercorp.fixturemonkey.FixtureMonkey
 import com.navercorp.fixturemonkey.kotlin.KFixtureMonkey
+import io.restassured.RestAssured
+import org.junit.jupiter.api.BeforeEach
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.boot.web.server.LocalServerPort
 
 const val REPEAT_COUNT = 5
 
 val fixture: FixtureMonkey = KFixtureMonkey.create()
 
-@SpringBootTest(classes = [CookiepangApplication::class])
+@SpringBootTest(classes = [CookiepangApplication::class], webEnvironment = RANDOM_PORT)
 abstract class SpringContextFixture {
-    val fixture: FixtureMonkey = com.ojicoin.cookiepang.fixture
+    @LocalServerPort
+    protected var port = 0
+
+    protected val fixture: FixtureMonkey = com.ojicoin.cookiepang.fixture
+
+    @BeforeEach
+    internal fun setUp() {
+        RestAssured.port = port
+    }
 }
