18dd1862f16daec2b17095c5556f7539e37bdff2
ah.jo <seongside@gmail.com>
2022-02-02 18:03:51 +0900

    [Nexters/FALCON#32] 유저 도메인으로 전환 (#16)

--- src/main/kotlin/com/ojicoin/cookiepang/domain/User.kt
+++ src/main/kotlin/com/ojicoin/cookiepang/domain/User.kt
@@ -8,11 +8,11 @@ import javax.validation.constraints.Size
 @Table("users")
 class User(
     @Id @Column("user_id") var id: Long?,
-    @Column("nickname") @field:Size(max = 100) val nickname: String,
-    @Column("introduction") @field:Size(max = 255) val introduction: String,
-    @Column("profile_url") @field:Size(max = 255) val profileUrl: String,
+    @Column("nickname") @field:Size(max = 100) var nickname: String,
+    @Column("introduction") @field:Size(max = 255) var introduction: String,
+    @Column("profile_url") @field:Size(max = 255) var profileUrl: String,
     @Column("wallet_address") @field:Size(max = 255) val walletAddress: String,
-    @Column("status") val status: UserStatus,
+    @Column("status") var status: UserStatus,
 )
 
 enum class UserStatus { ACTIVE }
