a6998e135dbb006d67b402e3a86f166adc8bc15f
ah.jo <ah.jo@navercorp.com>
2022-02-05 04:35:50 +0900

    [] 테스트 실패 수정4

--- src/test/kotlin/com/ojicoin/cookiepang/event/EventHandlerTest.kt
+++ src/test/kotlin/com/ojicoin/cookiepang/event/EventHandlerTest.kt
@@ -1,12 +1,11 @@
 package com.ojicoin.cookiepang.event
 
 import com.navercorp.fixturemonkey.kotlin.giveMeOne
-import com.ojicoin.cookiepang.REPEAT_COUNT
 import com.ojicoin.cookiepang.SpringContextFixture
 import com.ojicoin.cookiepang.repository.ViewCountRepository
 import org.assertj.core.api.BDDAssertions.then
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationEventPublisher
 
@@ -15,7 +14,7 @@ class EventHandlerTest(
     @Autowired val viewCountRepository: ViewCountRepository,
 ) : SpringContextFixture() {
 
-    @RepeatedTest(REPEAT_COUNT)
+    @Test
     fun handleViewCookieEvent() {
         // given
         val viewCookieEvent = fixture.giveMeOne<ViewCookieEvent>()
