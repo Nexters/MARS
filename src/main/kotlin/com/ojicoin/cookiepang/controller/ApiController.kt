8c5a1d5a0c2fceafb9df041564f90e9b736d5a44
ah.jo <ah.jo@navercorp.com>
2022-02-05 16:21:40 +0900

    [Nexters/FALCON#43] Swagger 추가 (#22)
    * [Nexters/FALCON#43] Swagger 추가
    
    * [] fix kt lint
    
    * [Nexters/FALCON#43] Add swagger
    
    * [Nexters/FALCON#43] fix ktlint
    
    * Apply requestbody for api
    
    Co-authored-by: baeji-hoon <baeji.hoon@linecorp.com>

--- src/main/kotlin/com/ojicoin/cookiepang/controller/ApiController.kt
+++ src/main/kotlin/com/ojicoin/cookiepang/controller/ApiController.kt
@@ -3,6 +3,15 @@ package com.ojicoin.cookiepang.controller
 import com.ojicoin.cookiepang.service.CookieService
 import com.ojicoin.cookiepang.service.InquiryService
 import com.ojicoin.cookiepang.service.UserTagService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.enums.ParameterIn
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.parameters.RequestBody
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import org.springdoc.core.annotations.RouterOperation
+import org.springdoc.core.annotations.RouterOperations
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Controller
 import org.springframework.web.servlet.function.RequestPredicates.GET
@@ -20,6 +29,17 @@ class ApiController(
     private val userTagService: UserTagService,
 ) {
     @Bean
+    @RouterOperation(
+        path = "/users/{userId}/cookies/{cookieId}",
+        operation = Operation(
+            operationId = "view",
+            parameters = [
+                Parameter(name = "userId", `in` = ParameterIn.PATH),
+                Parameter(name = "cookieId", `in` = ParameterIn.PATH),
+            ],
+            responses = [ApiResponse(responseCode = "200")]
+        ),
+    )
     fun view() = route(GET("/users/{userId}/cookies/{cookieId}")) {
         val userId = it.pathVariable("userId").toLong()
         val cookieId = it.pathVariable("cookieId").toLong()
@@ -28,6 +48,33 @@ class ApiController(
     }
 
     @Bean
+    @RouterOperations(
+        RouterOperation(
+            path = "/inquiries",
+            operation = Operation(
+                operationId = "inquiries",
+                requestBody = RequestBody(
+                    required = true,
+                    content = [Content(schema = Schema(implementation = InquiryRequestDto::class))]
+                ),
+                responses = [ApiResponse(responseCode = "200")]
+            ),
+        ),
+        RouterOperation(
+            path = "/users/{userId}/tags",
+            operation = Operation(
+                operationId = "createUserTags",
+                parameters = [
+                    Parameter(name = "userId", `in` = ParameterIn.PATH),
+                ],
+                requestBody = RequestBody(
+                    required = true,
+                    content = [Content(schema = Schema(implementation = UserTagCreateDto::class))]
+                ),
+                responses = [ApiResponse(responseCode = "200")]
+            ),
+        )
+    )
     fun create() = route(POST("/inquiries")) {
         // create inquiries
         val inquiryRequestDto = it.body<InquiryRequestDto>()
@@ -41,7 +88,7 @@ class ApiController(
         val userId = it.pathVariable("userId").toLong()
         val userTagCreateDto = it.body<UserTagCreateDto>()
 
-        userTagService.create(userId, userTagCreateDto.tagList)
+        userTagService.create(userId, userTagCreateDto.tagIdList)
 
         // TODO create certain uri path about created resource
         created(URI.create("")).build()
@@ -51,9 +98,9 @@ class ApiController(
 data class InquiryRequestDto(
     val title: String,
     val senderUserId: Long,
-    val receiverUserId: Long
+    val receiverUserId: Long,
 )
 
 data class UserTagCreateDto(
-    val tagList: List<Long>
+    val tagIdList: List<Long>,
 )
