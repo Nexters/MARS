fcef9d331982f622abc597041f514c99cbbc984f
baeji-hoon <baeji.hoon@linecorp.com>
2022-02-05 15:18:01 +0900

    Apply code review

--- src/main/kotlin/com/ojicoin/cookiepang/controller/ApiController.kt
+++ src/main/kotlin/com/ojicoin/cookiepang/controller/ApiController.kt
@@ -5,10 +5,10 @@ import com.ojicoin.cookiepang.service.InquiryService
 import com.ojicoin.cookiepang.service.UserTagService
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Controller
-import org.springframework.web.servlet.function.RequestPredicates
 import org.springframework.web.servlet.function.RequestPredicates.GET
+import org.springframework.web.servlet.function.RequestPredicates.POST
 import org.springframework.web.servlet.function.RouterFunctions.route
-import org.springframework.web.servlet.function.ServerResponse
+import org.springframework.web.servlet.function.ServerResponse.created
 import org.springframework.web.servlet.function.ServerResponse.ok
 import org.springframework.web.servlet.function.body
 import java.net.URI
@@ -20,7 +20,7 @@ class ApiController(
     private val userTagService: UserTagService,
 ) {
     @Bean
-    fun viewCookie() = route(GET("/users/{userId}/cookies/{cookieId}")) {
+    fun view() = route(GET("/users/{userId}/cookies/{cookieId}")) {
         val userId = it.pathVariable("userId").toLong()
         val cookieId = it.pathVariable("cookieId").toLong()
         val cookie = cookieService.view(userId = userId, cookieId = cookieId)
@@ -28,24 +28,23 @@ class ApiController(
     }
 
     @Bean
-    fun createInquiries() = route(RequestPredicates.POST("/inquiries")) {
+    fun create() = route(POST("/inquiries")) {
+        // create inquiries
         val inquiryRequestDto = it.body<InquiryRequestDto>()
 
         inquiryService.create(inquiryRequestDto.title, inquiryRequestDto.senderUserId, inquiryRequestDto.receiverUserId)
 
         // TODO create certain uri path about created resource
-        ServerResponse.created(URI.create("")).build()
-    }
-
-    @Bean
-    fun createUserTags() = route(RequestPredicates.POST("/users/{userId}/tags")) {
+        created(URI.create("")).build()
+    }.andRoute(POST("/users/{userId}/tags")) {
+        // create user interested tags
         val userId = it.pathVariable("userId").toLong()
         val userTagCreateDto = it.body<UserTagCreateDto>()
 
         userTagService.create(userId, userTagCreateDto.tagList)
 
         // TODO create certain uri path about created resource
-        ServerResponse.created(URI.create("")).build()
+        created(URI.create("")).build()
     }
 }
 
