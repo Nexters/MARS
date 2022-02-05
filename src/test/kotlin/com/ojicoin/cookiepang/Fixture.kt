dd4e471eb348fb5f110a8d0047a383429ca42d5e
ah.jo <ah.jo@navercorp.com>
2022-02-05 17:49:38 +0900

    [Nexters/FALCON#39] Event Customizer 추가

--- src/test/kotlin/com/ojicoin/cookiepang/Fixture.kt
+++ src/test/kotlin/com/ojicoin/cookiepang/Fixture.kt
@@ -1,19 +1,26 @@
 package com.ojicoin.cookiepang
 
 import com.navercorp.fixturemonkey.FixtureMonkey
-import com.navercorp.fixturemonkey.kotlin.KFixtureMonkey
+import com.navercorp.fixturemonkey.customizer.ArbitraryCustomizer
+import com.navercorp.fixturemonkey.generator.FieldArbitraries
+import com.navercorp.fixturemonkey.kotlin.KFixtureMonkeyBuilder
+import com.ojicoin.cookiepang.event.ViewCookieEvent
 import com.ojicoin.cookiepang.repository.ViewCountRepository
 import io.restassured.RestAssured
+import net.jqwik.api.Arbitraries
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.context.ApplicationEvent
 
 const val REPEAT_COUNT = 5
 
-val fixture: FixtureMonkey = KFixtureMonkey.create()
+val fixture: FixtureMonkey = KFixtureMonkeyBuilder()
+    .addCustomizer(ViewCookieEvent::class.java, ApplicationEventCustomizer())
+    .build()
 
 @SpringBootTest(classes = [CookiepangApplication::class], webEnvironment = RANDOM_PORT)
 abstract class SpringContextFixture {
@@ -35,3 +42,13 @@ abstract class SpringContextFixture {
         viewCountRepository.deleteAll() // TODO: 이벤트 초기화 처리로 변경
     }
 }
+
+class ApplicationEventCustomizer<T : ApplicationEvent> : ArbitraryCustomizer<T> {
+    override fun customizeFields(type: Class<T>?, fieldArbitraries: FieldArbitraries) {
+        fieldArbitraries.putArbitrary("source", Arbitraries.of(EmptyObject()))
+    }
+
+    override fun customizeFixture(`object`: T?): T? = `object`
+}
+
+class EmptyObject
