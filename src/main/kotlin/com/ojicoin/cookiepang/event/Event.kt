c0eb3fc4db3dd18fa48c867c272fcaac19bea0fc
ah.jo <ah.jo@navercorp.com>
2022-02-05 17:56:06 +0900

    [Nexters/FALCON#21] 쿠키를 불변 객체가 아닌 도메인으로 변경 (#15)

--- src/main/kotlin/com/ojicoin/cookiepang/event/Event.kt
+++ src/main/kotlin/com/ojicoin/cookiepang/event/Event.kt
@@ -1,7 +1,17 @@
 package com.ojicoin.cookiepang.event
 
+import org.springframework.context.ApplicationEvent
+
 class Event {
     // ktlint 에러 해결용
 }
 
-data class ViewCookieEvent(val userId: Long, val cookieId: Long)
+data class ViewCookieEvent(
+    private val source: Any,
+    val userId: Long,
+    val cookieId: Long,
+) : ApplicationEvent(source) {
+    override fun getSource(): Any {
+        return super.getSource()
+    }
+}
--- src/test/kotlin/com/ojicoin/cookiepang/service/CookieServiceTest.kt
+++ src/test/kotlin/com/ojicoin/cookiepang/service/CookieServiceTest.kt
@@ -5,6 +5,7 @@ import com.ojicoin.cookiepang.REPEAT_COUNT
 import com.ojicoin.cookiepang.SpringContextFixture
 import com.ojicoin.cookiepang.domain.Cookie
 import com.ojicoin.cookiepang.repository.CookieRepository
+import com.ojicoin.cookiepang.repository.ViewCountRepository
 import org.assertj.core.api.BDDAssertions.then
 import org.assertj.core.api.BDDAssertions.thenThrownBy
 import org.junit.jupiter.api.RepeatedTest
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired
 
 class CookieServiceTest(
     @Autowired val cookieRepository: CookieRepository,
+    @Autowired val viewCountRepository: ViewCountRepository,
     @Autowired val sut: CookieService,
 ) : SpringContextFixture() {
     @RepeatedTest(REPEAT_COUNT)
@@ -23,7 +25,9 @@ class CookieServiceTest(
 
         val expected = sut.view(userId = cookie.ownedUserId, cookieId = actual.id!!)
 
+        val viewCount = viewCountRepository.findByCookieId(expected.id!!)
         then(actual).isEqualTo(expected)
+        then(viewCount.count).isEqualTo(1)
     }
 
     @RepeatedTest(REPEAT_COUNT)
