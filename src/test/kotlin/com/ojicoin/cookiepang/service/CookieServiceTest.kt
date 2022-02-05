73b399001d4432e07d73275da37c95081673ae6a
ah.jo <ah.jo@navercorp.com>
2022-02-05 16:31:45 +0900

    [Nexters/FALCON#39] fix test

--- src/test/kotlin/com/ojicoin/cookiepang/service/CookieServiceTest.kt
+++ src/test/kotlin/com/ojicoin/cookiepang/service/CookieServiceTest.kt
@@ -4,30 +4,41 @@ import com.navercorp.fixturemonkey.kotlin.giveMeBuilder
 import com.ojicoin.cookiepang.REPEAT_COUNT
 import com.ojicoin.cookiepang.SpringContextFixture
 import com.ojicoin.cookiepang.domain.Cookie
+import com.ojicoin.cookiepang.domain.CookieStatus
+import com.ojicoin.cookiepang.domain.User
 import com.ojicoin.cookiepang.repository.CookieRepository
-import com.ojicoin.cookiepang.repository.ViewCountRepository
+import com.ojicoin.cookiepang.repository.UserRepository
 import org.assertj.core.api.BDDAssertions.then
 import org.assertj.core.api.BDDAssertions.thenThrownBy
 import org.junit.jupiter.api.RepeatedTest
 import org.springframework.beans.factory.annotation.Autowired
 
 class CookieServiceTest(
-    @Autowired val cookieRepository: CookieRepository,
-    @Autowired val viewCountRepository: ViewCountRepository,
     @Autowired val sut: CookieService,
+    @Autowired val cookieRepository: CookieRepository,
+    @Autowired val userRepository: UserRepository,
 ) : SpringContextFixture() {
     @RepeatedTest(REPEAT_COUNT)
     fun view() {
+        val creator = fixture.giveMeBuilder<User>()
+            .setNull("id")
+            .sample()
+        val collector = fixture.giveMeBuilder<User>()
+            .setNull("id")
+            .sample()
+        val authorUserId = userRepository.save(creator).id!!
+        val ownedUserId = userRepository.save(collector).id!!
         val cookie = fixture.giveMeBuilder<Cookie>()
             .setNull("id")
+            .set("authorUserId", authorUserId)
+            .set("ownedUserId", ownedUserId)
+            .set("status", CookieStatus.ACTIVE)
             .sample()
         val actual = cookieRepository.save(cookie)
 
         val expected = sut.view(userId = cookie.ownedUserId, cookieId = actual.id!!)
 
-        val viewCount = viewCountRepository.findByCookieId(expected.id!!)
         then(actual).isEqualTo(expected)
-        then(viewCount.count).isEqualTo(1)
     }
 
     @RepeatedTest(REPEAT_COUNT)
