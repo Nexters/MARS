07f760c7f87f396fe6d9ebf3ccefecf4cd686083
ah.jo <ah.jo@navercorp.com>
2022-02-05 18:04:36 +0900

    [Nexters/FALCON#39] remove useless

--- src/main/kotlin/com/ojicoin/cookiepang/service/CookieService.kt
+++ src/main/kotlin/com/ojicoin/cookiepang/service/CookieService.kt
@@ -1,7 +1,6 @@
 package com.ojicoin.cookiepang.service
 
 import com.ojicoin.cookiepang.domain.Cookie
-import com.ojicoin.cookiepang.event.ViewCookieEvent
 import com.ojicoin.cookiepang.repository.CookieRepository
 import org.springframework.stereotype.Service
 
@@ -10,15 +9,4 @@ class CookieService(
     private val cookieRepository: CookieRepository,
 ) {
     fun get(cookieId: Long): Cookie = cookieRepository.findById(cookieId).orElseThrow()
-
-    fun view(userId: Long, cookieId: Long): Cookie {
-        val cookie: Cookie = cookieRepository.findById(cookieId).orElseThrow()
-        if (cookie.ownedUserId != userId) {
-            throw IllegalArgumentException("$userId access other users cookie ${cookie.id}")
-        }
-
-        cookie.addEvent(ViewCookieEvent(source = cookie, userId = userId, cookieId = cookieId))
-        cookieRepository.save(cookie)
-        return cookie
-    }
 }
