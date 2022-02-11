62fb7c31e336368b9e29a45fc63b498b3019b15b
baeji-hoon <baeji.hoon@linecorp.com>
2022-02-12 01:37:08 +0900

    Apply code review

--- src/main/kotlin/com/ojicoin/cookiepang/service/AskService.kt
+++ src/main/kotlin/com/ojicoin/cookiepang/service/AskService.kt
@@ -1,7 +1,7 @@
 package com.ojicoin.cookiepang.service
 
-import com.ojicoin.cookiepang.domain.AskStatus.PENDING
 import com.ojicoin.cookiepang.domain.Ask
+import com.ojicoin.cookiepang.domain.AskStatus.PENDING
 import com.ojicoin.cookiepang.repository.AskRepository
 import org.springframework.stereotype.Service
 
@@ -11,8 +11,19 @@ class AskService(
 ) {
 
     fun create(title: String, senderUserId: Long, receiverUserId: Long): Ask {
+        if (senderUserId == receiverUserId) {
+            throw IllegalArgumentException("senderUserId is same receiverUserId. senderUserId=$senderUserId, receiverUserId=$receiverUserId")
+        }
+
         val savedAsk =
-            askRepository.save(Ask(title = title, status = PENDING, senderUserId = senderUserId, receiverUserId = receiverUserId))
+            askRepository.save(
+                Ask(
+                    title = title,
+                    status = PENDING,
+                    senderUserId = senderUserId,
+                    receiverUserId = receiverUserId
+                )
+            )
 
         // TODO make notification
         return savedAsk
