8bf981573118a29bec2f98e316d4e1f2da35db63
ah.jo <ah.jo@navercorp.com>
2022-02-17 23:43:55 +0900

    [] CORS 설정 추가

---
+++ src/main/kotlin/com/ojicoin/cookiepang/config/WebConfiguration.kt
@@ -0,0 +1,14 @@
+package com.ojicoin.cookiepang.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@Profile(value = ["local", "dev"])
+class WebConfiguration : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+    }
+}
