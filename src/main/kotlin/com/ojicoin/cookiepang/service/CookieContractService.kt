1fbb537f1cdbc43ab981016805a7f36ae244b0ae
강성찬[MYBOX Dev1] <seongchan.kang@navercorp.com>
2022-02-18 22:57:23 +0900

    [Nexter/FALCON#81] 와일드 카드 import 제거

--- src/main/kotlin/com/ojicoin/cookiepang/service/CookieContractService.kt
+++ src/main/kotlin/com/ojicoin/cookiepang/service/CookieContractService.kt
@@ -20,7 +20,7 @@ import org.web3j.tx.gas.DefaultGasProvider
 import java.math.BigInteger
 import java.time.Instant
 import java.time.LocalDateTime
-import java.util.*
+import java.util.TimeZone
 
 /**
  * @author seongchan.kang
