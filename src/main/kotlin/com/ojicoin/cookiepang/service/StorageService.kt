b4334ef4a8f412d287cdd383c5382dc598190feb
baeji-hoon <baeji.hoon@linecorp.com>
2022-02-19 17:33:16 +0900

    Integration s3 storage

--- src/main/kotlin/com/ojicoin/cookiepang/service/StorageService.kt
+++ src/main/kotlin/com/ojicoin/cookiepang/service/StorageService.kt
@@ -1,32 +1,30 @@
 package com.ojicoin.cookiepang.service
 
-import com.ojicoin.cookiepang.repository.FileSystemStorageRepository
+import com.ojicoin.cookiepang.repository.StorageRepository
 import org.springframework.stereotype.Service
 import java.io.InputStream
 import java.nio.file.Files
+import java.nio.file.Paths
 import java.util.UUID
-import kotlin.io.path.Path
 
 @Service
 class StorageService(
-    val storageRepository: FileSystemStorageRepository
+    private val storageRepository: StorageRepository
 ) {
 
+    private val userDirectory = "users"
+
     fun saveProfilePicture(userId: Long, fileName: String, inputStream: InputStream): String {
-        val key = "${UUID.randomUUID()}.${getFileExtension(fileName)}"
-        return storageRepository.store(key = makeProfilePicturePath(userId, key), inputStream = inputStream)
+        val keyWithUserDirectory = "$userDirectory/$userId/${UUID.randomUUID()}.${getFileExtension(fileName)}"
+
+        return storageRepository.store(key = keyWithUserDirectory, inputStream = inputStream)
     }
 
     // ref: https://github.com/JetBrains/kotlin/blob/master/libraries/stdlib/jvm/src/kotlin/io/files/Utils.kt#L88-L92
     private fun getFileExtension(fileName: String) = fileName.substringAfterLast('.', "")
 
-    // path format "users/{userId}/pictures/{pictureName}"
-    private fun makeProfilePicturePath(userId: Long, pictureName: String): String {
-        return "users/$userId/pictures/$pictureName"
-    }
-
-    // This method is temporary for getting picture in localhost
+    // This method is temporary for getting picture in local env
     fun getProfilePicture(userId: Long, pictureName: String): ByteArray {
-        return Files.readAllBytes(Path(makeProfilePicturePath(userId, pictureName)))
+        return Files.readAllBytes(Paths.get("$userDirectory/$userId/$pictureName"))
     }
 }
