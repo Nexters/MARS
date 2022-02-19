53758afa054722fe4aecc364a5a6b62bed37db06
강성찬[MYBOX Dev1] <seongchan.kang@navercorp.com>
2022-02-19 18:10:45 +0900

    [Nexters/FALCON#noissue] retry fallback 추가 (최대 10초까지 재시도)

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("org.springframework.retry:spring-retry")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
