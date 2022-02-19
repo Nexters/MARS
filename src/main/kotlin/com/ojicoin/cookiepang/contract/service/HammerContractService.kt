195e22a0a7cad5974ecf4ee02f695260d918ab3e
강성찬(kohen) <tjdcks1781@gmail.com>
2022-02-19 21:46:39 +0900

    [Nexters/FALCON#96] coin contract -> hammer contract, hammer 지급 및 갯수조회 기능 추가 (#66)
    Co-authored-by: 강성찬[MYBOX Dev1] <seongchan.kang@navercorp.com>

---
+++ src/main/kotlin/com/ojicoin/cookiepang/contract/service/HammerContractService.kt
@@ -0,0 +1,61 @@
+package com.ojicoin.cookiepang.contract.service
+
+import com.klaytn.caver.abi.datatypes.Type
+import com.klaytn.caver.contract.Contract
+import com.klaytn.caver.contract.SendOptions
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import org.web3j.tx.gas.DefaultGasProvider
+import java.math.BigInteger
+import java.util.Arrays
+
+/**
+ * @author seongchan.kang
+ */
+@Service
+class HammerContractService(
+    private val coinContract: Contract,
+    @Value("\${contract.admin-address}") val adminAddress: String,
+) {
+
+    enum class CoinContractMethod(val methodName: String) {
+        IS_MAX_APPROVED_ADDRESS("maxApprovedAddress"),
+        BALANCE_OF("balanceOf"),
+        SAFE_TRANSFER("safeTransfer");
+    }
+
+    // FIXME: 커스텀 익셉션 추가
+    fun isMaxApprovedAddress(address: String): Boolean {
+        return try {
+            val callResult: List<Type<*>> = coinContract.call(CoinContractMethod.IS_MAX_APPROVED_ADDRESS.methodName, address)
+            val result: Type<Boolean> = callResult[0] as Type<Boolean>
+            result.value
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw RuntimeException()
+        }
+    }
+
+    fun balanceOf(address: String): BigInteger {
+        return try {
+            val callResult = coinContract.call(CoinContractMethod.BALANCE_OF.methodName, address)
+            val result: Type<BigInteger> = callResult[0] as Type<BigInteger>
+            result.value
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+            throw RuntimeException()
+        }
+    }
+
+    // 이벤트로 망치 지급할때 사용 amount는 decimal 18 단위 기준 (즉, amount 10^18 = 1hammer)
+    fun sendHammer(address: String, amount: BigInteger) {
+        try {
+            val sendOptions = SendOptions(adminAddress, DefaultGasProvider.GAS_LIMIT)
+            val functionParams = Arrays.asList<Any>(address, amount)
+            coinContract.getMethod(CoinContractMethod.SAFE_TRANSFER.methodName).send(functionParams, sendOptions)
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+            throw RuntimeException()
+        }
+    }
+}
--- src/test/kotlin/com/ojicoin/cookiepang/contract/service/HammerContractServiceTest.kt
+++ src/test/kotlin/com/ojicoin/cookiepang/contract/service/HammerContractServiceTest.kt
@@ -3,19 +3,24 @@ package com.ojicoin.cookiepang.contract.service
 import com.ojicoin.cookiepang.SpringContextFixture
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import java.math.BigInteger
 
 /**
  * @author seongchan.kang
  */
-internal class CoinContractServiceTest(
-    @Autowired val sut: CoinContractService
+internal class HammerContractServiceTest(
+    @Autowired val sut: HammerContractService
 ) : SpringContextFixture() {
-
-    // FIXME: 테스트 어떻게!?
 
     @Test
     fun isMaxApprovedAddress() {
         val isMaxApprovedAddress: Boolean = sut.isMaxApprovedAddress("0xE84deA09A59C6b614c4934c46aa13ec56f6f3fEa")
         println(isMaxApprovedAddress)
+    }
+
+    @Test
+    fun balanceOf() {
+        val balance: BigInteger = sut.balanceOf("0xE84deA09A59C6b614c4934c46aa13ec56f6f3fEa")
+        println(balance)
     }
 }
