527e31900fe12aa38220799e59444db5f8414934
강성찬 <tjdcks1781@gmail.com>
2022-02-22 02:23:02 +0900

    [Nexters/FALCON#noissue] cors

--- src/main/kotlin/com/ojicoin/cookiepang/config/WebConfiguration.kt
+++ src/main/kotlin/com/ojicoin/cookiepang/config/WebConfiguration.kt
@@ -2,13 +2,22 @@ package com.ojicoin.cookiepang.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.http.HttpMethod
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-@Profile(value = ["local", "dev, real"])
+@Profile(value = ["local", "dev", "real"])
 class WebConfiguration : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods(
+                HttpMethod.GET.name,
+                HttpMethod.HEAD.name,
+                HttpMethod.POST.name,
+                HttpMethod.PUT.name,
+                HttpMethod.DELETE.name
+            )
     }
 }
