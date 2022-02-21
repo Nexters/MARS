861855d910646f46eab7f8e795a395a23db03ca2
강성찬 <tjdcks1781@gmail.com>
2022-02-22 00:29:06 +0900

    [Nexters/FALCON#noissue] cors 설정 real 추가

--- src/main/kotlin/com/ojicoin/cookiepang/config/WebConfiguration.kt
+++ src/main/kotlin/com/ojicoin/cookiepang/config/WebConfiguration.kt
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-@Profile(value = ["local", "dev"])
+@Profile(value = ["local", "dev, real"])
 class WebConfiguration : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
