05a83330e93c43451013de3c9f0aff5db6f6e9ca
ah.jo <ah.jo@navercorp.com>
2022-02-25 01:40:29 +0900

    [#127] 로깅 추가 (#127)

--- src/main/kotlin/com/ojicoin/cookiepang/config/WebConfiguration.kt
+++ src/main/kotlin/com/ojicoin/cookiepang/config/WebConfiguration.kt
@@ -1,10 +1,24 @@
 package com.ojicoin.cookiepang.config
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpMethod
+import org.springframework.stereotype.Component
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.util.ContentCachingRequestWrapper
+import org.springframework.web.util.ContentCachingResponseWrapper
+import java.util.Enumeration
+import javax.servlet.Filter
+import javax.servlet.FilterChain
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import kotlin.system.measureTimeMillis
 
 @Configuration
 @Profile(value = ["local", "dev", "real"])
@@ -19,5 +33,41 @@ class WebConfiguration : WebMvcConfigurer {
                 HttpMethod.PUT.name,
                 HttpMethod.DELETE.name
             )
+    }
+}
+
+@Component
+class LoggingFilter : Filter {
+    private val objectMapper = jacksonObjectMapper()
+    private val log: Logger = LoggerFactory.getLogger(LoggingFilter::class.java)
+
+    override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
+        val cachedRequest = ContentCachingRequestWrapper(request as HttpServletRequest)
+        val cachedResponse = ContentCachingResponseWrapper(response as HttpServletResponse)
+
+        val time = measureTimeMillis { chain.doFilter(cachedRequest, cachedResponse) }
+
+        val loggingMessage = """ [REQUEST] ${request.method} ${request.requestURI} ${cachedResponse.status} - ${time}ms
+                Headers : ${getHeaders(request)}
+                RequestBody : ${objectMapper.readTree(cachedRequest.contentAsByteArray)}
+                ResponseBody : ${objectMapper.readTree(cachedResponse.contentAsByteArray)}
+        """.trimIndent()
+
+        if (cachedResponse.status >= 400) {
+            log.error(loggingMessage)
+        } else {
+            log.info(loggingMessage)
+        }
+        cachedResponse.copyBodyToResponse()
+    }
+
+    private fun getHeaders(request: HttpServletRequest): Map<String, *> {
+        val headerMap = mutableMapOf<String, Any>()
+        val headerArray: Enumeration<String> = request.headerNames
+        while (headerArray.hasMoreElements()) {
+            val headerName = headerArray.nextElement() as String
+            headerMap[headerName] = request.getHeader(headerName)
+        }
+        return headerMap
     }
 }
