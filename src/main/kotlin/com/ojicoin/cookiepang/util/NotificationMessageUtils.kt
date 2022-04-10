6a2da5d9f932821f042d85a3bf930518997c0b27
ah.jo <ah.jo@navercorp.com>
2022-04-10 20:51:29 +0900

    [NO-ISSUE] util 메소드 Spring Context 의존성 제거

--- src/main/kotlin/com/ojicoin/cookiepang/util/NotificationMessageUtils.kt
+++ src/main/kotlin/com/ojicoin/cookiepang/util/NotificationMessageUtils.kt
@@ -1,18 +1,17 @@
 package com.ojicoin.cookiepang.util
 
-import org.springframework.stereotype.Component
-
-@Component
 class NotificationMessageUtils {
-    fun getAskMessage(cookieTitle: String): String {
-        // TODO consider supporting other language
+    companion object {
+        fun getAskMessage(cookieTitle: String): String {
+            // TODO consider supporting other language
 
-        return "익명의 누군가가 ${cookieTitle}라는 질문을 요청했습니다."
-    }
+            return "익명의 누군가가 ${cookieTitle}라는 질문을 요청했습니다."
+        }
 
-    fun getTransactionMessage(nickname: String, cookieTitle: String, hammerCount: Long): String {
-        // TODO consider supporting other language
+        fun getTransactionMessage(nickname: String, cookieTitle: String, hammerCount: Long): String {
+            // TODO consider supporting other language
 
-        return "${nickname}님이 ${cookieTitle}를 망치 ${hammerCount}톤으로 구매했습니다."
+            return "${nickname}님이 ${cookieTitle}를 망치 ${hammerCount}톤으로 구매했습니다."
+        }
     }
 }
