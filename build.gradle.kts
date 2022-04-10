6c6f9801a3acf6ff57a889362956b92ecf510b9a
ah.jo <ah.jo@navercorp.com>
2022-04-10 21:45:36 +0900

    [NO-ISSUE] version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ val restAssuredVersion: String by project
 val springDocVersion: String by project
 
 plugins {
-    id("org.springframework.boot") version "2.6.3"
+    id("org.springframework.boot") version "2.6.6"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
